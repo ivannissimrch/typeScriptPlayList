01dfb921e9f60dd5e1430ecb5b765870d63848ef
Ivan Rebolledo <106914406+ivannissimrch@users.noreply.github.com>
2024-07-01 20:16:39 -0400

    Update src/helpers/getTokenFromUrl.ts
    Co-authored-by: xMartin <m@xmmail.de>

--- src/helpers/getTokenFromUrl.ts
+++ src/helpers/getTokenFromUrl.ts
@@ -1,17 +1,13 @@
 // This is part of the authentication code used to obtain a token from Spotify during login.
-interface Token {
-  [key: string]: string;
-}
-
 export default function getTokenFromUrl() {
-  return window.location.hash
+  const urlHashParams = window.location.hash
     .substring(1)
     .split("&")
-    .reduce((initial: Token, item: string) => {
-      console.log(initial);
-      console.log(item);
+    .reduce((initial, item) => {
       const parts = item.split("=");
       initial[parts[0]] = decodeURIComponent(parts[1]);
       return initial;
-    }, {});
+    }, {} as Record<string, string | undefined>);
+  
+    return urlHashParams.access_token;
 }
